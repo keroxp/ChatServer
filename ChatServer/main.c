b73c0820338d1fb533b1e5c56dcd8a7295cb922b
Yusuke Sakurai <kerokerokerop@gmail.com>
2013-07-08 11:21:52 +0900

    Initial Commit

---
+++ ChatServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ChatServer
+//
+//  Created by 桜井雄介 on 2013/07/08.
+//  Copyright (c) 2013年 Yusuke Sakurai. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
